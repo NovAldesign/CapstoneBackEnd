0eed3dd2bd19d196e9092e628b2fc2c236982713
NovAldesign <williams.lavon@gmail.com>
2026-03-02 15:48:11 -0500

    completed imports and setups in server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Imports
-
+import express from "express";
+import dotenv from "dotenv";
 
 
 // Setups
-
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Middlewares (Request)
 
