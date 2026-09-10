31593272dd434883713f5666b24546679bc768fd
NovAldesign <williams.lavon@gmail.com>
2026-09-10 12:33:20 -0400

    updated articleRoute

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,6 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const Article = require('../models/article');
+
+// POST route to create a new article
 router.post('/', async (req, res) => {
   const { title, slug, content, excerpt, category, imageUrl } = req.body;
-  
+
   const article = new Article({
     title,
     slug,
@@ -17,3 +22,5 @@ router.post('/', async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+module.exports = router;
